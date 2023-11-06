5066d90051cd3459e0b4ca8e5136edeea6b6245c
mahsin <mahsin1912@gmail.com>
2023-11-06 17:08:56 +0600

    jsonwebtoken dependency dwonload

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken")
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const { JsonWebTokenError } = require("jsonwebtoken");
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
